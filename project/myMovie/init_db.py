49e12a9d642dc050a78a9cb42b8d0637b1beaf70
sesame86 <61831933+sesame86@users.noreply.github.com>
2020-08-17 00:05:36 +0900

    2020-08-16

--- project/myMovie/init_db.py
+++ project/myMovie/init_db.py
@@ -51,7 +51,7 @@ def insert_movies(url):
 
 # 기존 mystar 콜렉션을 삭제하고, 출처 url들을 가져온 후, 크롤링하여 DB에 저장합니다.
 def insert_all():
-    db.movies.drop()  # mystar 콜렉션을 모두 지워줍니다.
+    db.naver_movies.drop()  # mystar 콜렉션을 모두 지워줍니다.
     urls = get_urls()
     for url in urls:
         insert_movies(url)

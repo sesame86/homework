2ae02f00f52d7700b7e415675918d87b270492b0
sesame86 <61831933+sesame86@users.noreply.github.com>
2020-08-17 20:43:35 +0900

    2020-08-17

--- project/myMovie/app.py
+++ project/myMovie/app.py
@@ -24,22 +24,7 @@ def show_movies():
     # 2. 성공하면 success 메시지와 함께 stars_list 목록을 클라이언트에 전달합니다.
     return jsonify(result)
 
-@app.route('/api/score', methods=['POST'])
-def movie_score():
-    # 1. 클라이언트가 전달한 name_give를 name_receive 변수에 넣습니다.
-    movie_title_recieve = request.form['movie_title_give']
 
-    # 2. mystar 목록에서 find_one으로 name이 name_receive와 일치하는 star를 찾습니다.
-    movie_score = db.naver_movies.find_one({'title': movie_title_recieve})
-    # 3. star의 like 에 1을 더해준 new_like 변수를 만듭니다.
-    new_score = movie_score['star'].value
-
-    # 4. mystar 목록에서 name이 name_receive인 문서의 like 를 new_like로 변경합니다.
-    # 참고: '$set' 활용하기!
-    db.member_score.update_one({'title': movie_title_recieve}, {'$set': {'like': new_score}})
-
-    # 5. 성공하면 success 메시지를 반환합니다.
-    return jsonify({'result': 'success'})
 
 
 if __name__ == '__main__':
